304a0b8700d5ad63ea8faeab08d3ee03563096e6
S0meth1ng85 <57530486+S0meth1ng85@users.noreply.github.com>
2021-09-23 15:33:36 +0200

    Update releasenotes.groovy

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -6,7 +6,7 @@ import hudson.model.*;
 
 @NonCPS
 def call(Map config=[:]){
-    def dir = new File(pwd());
+    def dir = new File('/var/jenkins_home/workspace/AutomatingGroovy/');
     
     new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
     { 
